fb910cea8bf66bbe6249cb7cd656aaebab0af392
Ruslan Sanigullin <hirudorar07@gmail.com>
2024-08-15 04:31:14 +0500

    fix code & building

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -5,13 +5,12 @@ from brain_games.cli import welcome_user
 
 
 def gcd(a, b):
-    if a == 0 or b == 0:
-        return(max(a, b))
-    else:
-        if a > b:
-            return gcd(a - b, b)
+    while a != 0 and b != 0:
+        if (a > b):
+            a = a % b
         else:
-            return gcd(a, b - a)
+            b = b % a
+    return a + b
 
 
 def main():
@@ -19,14 +18,18 @@ def main():
     print('Find the greatest common divisor of given numbers.')
     score = 0
     while (score < 3):
-        a = random.randint(0, 100)
-        b = random.randint(0, 100)
+        a = random.randint(0, 50)
+        b = random.randint(0, 50)
         print(f'Question: {a} {b}')
-        answer = input('Your answer: ')
-        if answer == gcd(a, b):
+        correct = gcd(a, b)
+        answer = int(input('Your answer: '))
+        print(answer)
+        if answer == correct:
+            score += 1
             print("Correct!")
         else:
-            print(f"'{answer} is wrong answer ;(. Correct answer was '{gcd(a, b)}'")
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct}'")
+            break
     if score == 3:
         print(f"Congratulations, {name}!")
 
