589407218eb3dbbfd86d2e92baa5dec5644451a9
Ruslan Sanigullin <hirudorar07@gmail.com>
2024-08-23 05:02:01 +0500

    add progression.py to brain_games && add code && build

---
+++ brain_games/progression.py
@@ -0,0 +1,43 @@
+import random
+from brain_games.cli import hi, number_count, check, win, lose, reply
+
+
+def text():
+    print('What number is missing in the progression?')
+    return
+
+
+def generate_progression():
+    start = random.randint(1, 10)
+    step = random.randint(1, 7)
+    progression = [start + i * step for i in range(10)]
+    return progression
+
+
+def hide_number(progression):
+    index = random.randint(0, len(progression) - 1)
+    hidden_number = progression[index]
+    progression[index] = '..'
+    print("Question: ", *progression)
+    return int(hidden_number)
+
+
+def progress_game():
+    name = hi()
+    cycle = number_count()
+    count = 0
+
+    text()
+
+    for _ in range(cycle):
+        progression = generate_progression()
+        answer = hide_number(progression)
+        user_answer = int(reply())
+        if check(user_answer, answer):
+            count += 1
+            if count == cycle:
+                win(name)
+                return
+        else:
+            lose(name, user_answer, answer)
+            return
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,39 +1,10 @@
 #!/usr/bin/env python3
 
-import random
-from brain_games.cli import hi, user_answer, check, number_count, end
-
-
-def generate_progression():
-    start = random.randint(1, 10)
-    step = random.randint(1, 7)
-    progression = [start + i * step for i in range(10)]
-    return progression
-
-
-def hide_number(progression):
-    index = random.randint(0, len(progression) - 1)
-    hidden_number = progression[index]
-    progression[index] = '..'
-    return progression, hidden_number
+from brain_games.progression import progress_game
 
 
 def main():
-    name = hi()
-    count = number_count()
-    correct_count = 0
-    print('What number is missing in the progression?')
-    for _ in range(count):
-        progression = generate_progression()
-        progression, hidden = hide_number(progression)
-        print("Question:", *progression)
-        answer = int(user_answer())
-        if check(name, answer, hidden):
-            correct_count += 1
-            if end(name, correct_count):
-                break
-        else:
-            break
+    progress_game()
 
 
 if __name__ == "__main__":
