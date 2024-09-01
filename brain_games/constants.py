f6574b0f56035f66753a22fe65b4e59efd67272a
Ruslan Sanigullin <hirudorar07@gmail.com>
2024-09-01 15:16:45 +0300

    edit code

---
+++ brain_games/constants.py
@@ -0,0 +1,5 @@
+def constant(func):
+    if func == "is_even":
+        return 'Answer "yes" if the number is even, otherwise answer "no".'
+    else:
+        return
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,7 +5,6 @@ from brain_games.even import is_even
 
 
 def main():
-    play()
     text: str = 'Answer "yes" if the number is even, otherwise answer "no".'
     progress_game(is_even, text)
 
